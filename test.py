53a3db1db3745abf5a1d07712a2f691765245bd7
Mark Campbell <nitrodist@gmail.com>
2020-03-30 09:55:31 -0500

    Test python project with KG

---
+++ test.py
@@ -0,0 +1,14 @@
+import json
+
+with open('account_mapping.json') as file_object:
+        data = json.load(file_object)
+print(data)
+
+vendor_name = "Joe Blow vendor"
+
+print data[vendor_name]
+
+if data[vendor_name] != None:
+    print "Found " + str(data[vendor_name]) + "!"
+else:
+    print "vendor '" + vendor_name + "' not found"
